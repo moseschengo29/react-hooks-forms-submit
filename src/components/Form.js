d34630961451a52612ad8239a7d6db3cad29b239
Moses <mochengo29@gmail.com>
2023-07-20 16:31:33 +0300

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,51 @@
 import React, { useState } from "react";
 
 function Form(props) {
+  const [users, setUsers] = useState([]);
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  function addUser(user) {
+    setUsers((users) => [...users, user]);
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      firstName,
+      lastName,
+      id: crypto.randomUUID(),
+    };
+    addUser(newUser);
+    console.log(newUser);
+
+    setFirstName("");
+    setLastName("");
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setFirstName(e.target.value)}
+          value={firstName}
+        />
+        <input
+          type="text"
+          onChange={(e) => setLastName(e.target.value)}
+          value={lastName}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <ol>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.firstName} {user.lastName}
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 }
 
